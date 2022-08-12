ff4a1d686b0fb73a0a95d515e08eeaacb88b1b3a
Remy Stewart <75761497+remypstewart@users.noreply.github.com>
2022-08-12 07:12:55 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+
+st.header("test html import")
+HtmlFile = open("./html/bar_viz.html", 'r', encoding='utf-8')
+source_code = HtmlFile.read() 
+components.html(source_code, height = 900)
